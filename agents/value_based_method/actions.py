86251e5395ab1dd0c9a87d9040121ee3cb43c6cf
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-09-16 17:30:28 -0300

    creating a class to represent discretize actions on value_based agent

---
+++ agents/value_based_method/actions.py
@@ -0,0 +1,15 @@
+import numpy as np
+from itertools import product
+
+
+class DiscretizeAction():
+    DISCRETE_VALUES = [0, 200, 360, 404, 440, 700, 900]
+    ACTIONS = np.array(list(product(*((DISCRETE_VALUES,)*4))))
+
+    @classmethod
+    def get(cls, id):
+        return cls.ACTIONS[id]
+
+    @classmethod
+    def size(cls):
+        return len(cls.ACTIONS)
