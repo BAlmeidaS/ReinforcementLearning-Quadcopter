d4d52e7dc9d98512afced5333a9355eeaa79c452
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-09-16 21:49:40 -0300

    restricting a lot actions in value based method

--- agents/value_based_method/actions.py
+++ agents/value_based_method/actions.py
@@ -3,13 +3,29 @@ from itertools import product
 
 
 class ActionSpace():
-    DISCRETE_VALUES = [0, 200, 360, 404, 440, 700, 900]
-    ACTIONS = np.array(list(product(*((DISCRETE_VALUES,)*4))))
+    # DISCRETE_VALUES = [0, 200, 360, 404, 440, 700, 900]
+    # ACTIONS = np.array(list(product(*((DISCRETE_VALUES,)*4))))
+    ACTIONS = np.array([[404, 404, 404, 404],
+                        [500, 500, 500, 500],
+                        [300, 300, 300, 300],
+                        [100, 100, 100, 100],
+                        [500, 500, 404, 404],
+                        [404, 500, 500, 404],
+                        [404, 404, 500, 500],
+                        [500, 404, 404, 500],
+                        [500, 500, 450, 450],
+                        [450, 500, 500, 450],
+                        [450, 450, 500, 500],
+                        [500, 450, 450, 500]])
     size = len(ACTIONS)
 
     @classmethod
     def get(cls, id):
         return cls.ACTIONS[id]
+
+    @classmethod
+    def find(cls, action):
+        return np.where((cls.ACTIONS == action).all(axis=1))[0][0]
 
     @classmethod
     def sample(cls):
--- agents/value_based_method/agent.py
+++ agents/value_based_method/agent.py
@@ -20,7 +20,7 @@ class TiledQTable:
         self.tilings = create_tilings(low, high, tiling_specs)
         self.state_sizes = [tuple(len(splits)+1 for splits in tiling_grid) for tiling_grid in self.tilings]
         self.action_size = action_size
-        self.q_tables = [QTable(state_size, self.action_size) for state_size in self.state_sizes]
+        self.q_tables = [QTable(state_size, action_size) for state_size in self.state_sizes]
         print("TiledQTable(): no. of internal tables = ", len(self.q_tables))
 
     def get(self, state, action):
@@ -36,7 +36,7 @@ class TiledQTable:
             q_table.q_table[i + (action, )] = alpha * value + (1.0 - alpha) * q_s
 
 
-class Basic_Agent():
+class AgentSarsamax():
     def __init__(self, task, gamma=0.99, alpha=0.1):
         # constraints
         low_boundaries = (-30, -30, 0, -1, -1, -1)
@@ -75,8 +75,13 @@ class Basic_Agent():
         self.gamma = gamma
         self.alpha = alpha
 
+        #scores
+        self.best_score = -np.inf
+        self.score = 0
 
-    def act(self, state, epsilon):
+    def act(self, raw_state, epsilon):
+        state = self._preprocess_state(raw_state)
+
         self.last_state = state
 
         do_exploration = np.random.uniform(0, 1) < epsilon
@@ -85,17 +90,30 @@ class Basic_Agent():
         else:
             return ActionSpace.get(np.argmax(self._greedy_policy(state)))
 
-    def step(self, next_state, action, reward):
+    def step(self, raw_next_state, raw_action, reward, done):
         """
         This is an aplication of sarsamax
         """
         if self.last_state is None:
             raise RuntimeError('trying to reward an action without running the agent yet!')
 
+        next_state = self._preprocess_state(raw_next_state)
+        action = ActionSpace.find(raw_action)
+
         value = reward + self.gamma * max(self._greedy_policy(next_state))
 
         self.q_table.update(self.last_state, action, value, alpha=self.alpha)
 
+        self.score += reward
+
+        if done:
+            if self.score > self.best_score:
+                self.best_score = self.score
+
+    def _preprocess_state(self, raw_state):
+        last_state = raw_state[-6:]
+        sin_of_angles = np.sin(last_state[-3:])
+        return np.concatenate((last_state[:3], sin_of_angles))
 
     def _greedy_policy(self, state):
         return [self.q_table.get(state, a) for a in range(self.actions)]
